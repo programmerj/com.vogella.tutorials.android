2a0c0afbb3e5b3702e7fdc96a503650e7f109d10
Patrick Moorehouse <patmoorehouse.irl@gmail.com>
2016-10-12 11:46:06 +0100

    Update to pass input text via Intent
    Unit tests were testing whether the text passed to second activity matched what is entered into the text field of first activity, but the text was never passed to second activity, so tests failed. Now MainActivity takes the text from text field and adds as an Extra to the intent used to start second activity.

--- AndroidTestingEspresso/res/simple/MainActivity.java
+++ AndroidTestingEspresso/res/simple/MainActivity.java
@@ -7,22 +7,26 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editText = (EditText) findViewById(R.id.inputField);
     }
 
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.changeText:
-                EditText editText = (EditText) findViewById(R.id.inputField);
                 editText.setText("Lalala");
                 break;
             case R.id.switchActivity:
                 Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("input", editText.getText().toString());
                 startActivity(intent);
                 break;
         }
