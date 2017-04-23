ba761a30e653fc5d44cad85906bb603c3b5fc4aa
Lars Vogel <Lars.Vogel@vogella.com>
2017-04-24 00:59:19 +0200

    Updates on Recycler view

--- AndroidRecyclerView/res/recyclerview/MyAdapter.java
+++ AndroidRecyclerView/res/recyclerview/MyAdapter.java
@@ -1,81 +1,44 @@
-package com.vogella.android.recyclerview;
+package com.vogella.android.databinding;
 
-import java.util.ArrayList;
-
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-	private ArrayList<String> mDataset;
+import java.util.Arrays;
+import java.util.List;
 
-	// Provide a reference to the views for each data item
-	// Complex data items may need more than one view per item, and
-	// you provide access to all the views for a data item in a view holder
-	public class ViewHolder extends RecyclerView.ViewHolder {
-		// each data item is just a string in this case
-		public TextView txtHeader;
-		public TextView txtFooter;
+public class SecondActivity extends Activity implements MainActivityContract.View {
 
-		public ViewHolder(View v) {
-			super(v);
-			txtHeader = (TextView) v.findViewById(R.id.firstLine);
-			txtFooter = (TextView) v.findViewById(R.id.secondLine);
-		}
-	}
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-	public void add(int position, String item) {
-		mDataset.add(position, item);
-		notifyItemInserted(position);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-	public void remove(String item) {
-		int position = mDataset.indexOf(item);
-		mDataset.remove(position);
-		notifyItemRemoved(position);
-	}
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
 
-	// Provide a suitable constructor (depends on the kind of dataset)
-	public MyAdapter(ArrayList<String> myDataset) {
-		mDataset = myDataset;
-	}
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
 
-	// Create new views (invoked by the layout manager)
-	@Override
-	public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-			int viewType) {
-		// create a new view
-		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
-		// set the view's size, margins, paddings and layout parameters
-		ViewHolder vh = new ViewHolder(v);
-		return vh;
-	}
+        List<String> items = Arrays.asList("Test1", "Test2"); // here the example data is created
 
-	// Replace the contents of a view (invoked by the layout manager)
-	@Override
-	public void onBindViewHolder(ViewHolder holder, int position) {
-		// - get element from your dataset at this position
-		// - replace the contents of the view with that element
-		final String name = mDataset.get(position);
-		holder.txtHeader.setText(mDataset.get(position));
-		holder.txtHeader.setOnClickListener(new OnClickListener() {
-		        @Override
-		        public void onClick(View v) {
-		        	remove(name);
-		        }
-		    });
-		
-		holder.txtFooter.setText("Footer: " + mDataset.get(position));
+        // define an adapter
+        mAdapter = new MyAdapter(items);
+        recyclerView.setAdapter(mAdapter);
+    }
 
-	}
-
-	// Return the size of your dataset (invoked by the layout manager)
-	@Override
-	public int getItemCount() {
-		return mDataset.size();
-	}
-
+    @Override
+    public void showData(TemperatureData temperatureData) {
+        String celsius = temperatureData.getCelsius();
+        Toast.makeText(this, celsius, Toast.LENGTH_SHORT).show();
+    }
 }
